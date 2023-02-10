dff1194b2a2ec7bee8478ca8bbcf07bda286977c
Fanny Campos <fannycamposa@gmail.com>
2023-02-09 21:59:39 -0300

    Logo

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -4,3 +4,12 @@
    Pagina Principal
 @endsection
 
+@section('contenido')
+
+<div class="container text-center">
+   <h1>App SSMO</h1>
+   <img src="{{ asset('img/logoprincipal.png') }}" alt="Logo Servicio de Salud Metropolitano">
+</div>
+
+@endsection
+
